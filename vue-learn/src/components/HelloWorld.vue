ac7f458a2997194d73c1b0337ae0de89a97a49f2
zhuqichen <zhuqichen972@163.com>
2020-07-24 17:29:16 +0800

    添加并测试vue拖拽插件vue-draggable-resizable例子和文档

--- vue-learn/src/components/HelloWorld.vue
+++ vue-learn/src/components/HelloWorld.vue
@@ -29,6 +29,12 @@
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
     <img id="text-img" alt="Vue logo" src="../assets/mz.jpg">
+    <div style="height: 500px; width: 500px; border: 1px solid red; position: relative;">
+      <vue-draggable-resizable :w="100" :h="100" @dragging="onDrag" @resizing="onResize" :parent="true">
+        <p>Hello! I'm a flexible component. You can drag me around and you can resize me.<br>
+          X: {{ x }} / Y: {{ y }} - Width: {{ width }} / Height: {{ height }}</p>
+      </vue-draggable-resizable>
+    </div>
   </div>
 </template>
 
@@ -43,6 +49,14 @@ export default {
   props: {
     msg: String
   },
+  data: function () {
+    return {
+      width: 0,
+      height: 0,
+      x: 0,
+      y: 0
+    }
+  },
   methods:{
     recognize: async () => {
       const img = document.getElementById('text-img');
@@ -55,6 +69,16 @@ export default {
       });
       const { data: { text } } = await worker.recognize(img);
       console.log(text);
+    },
+    onResize: function (x, y, width, height) {
+      this.x = x
+      this.y = y
+      this.width = width
+      this.height = height
+    },
+    onDrag: function (x, y) {
+      this.x = x
+      this.y = y
     }
   }
 }
--- vue-learn/src/main.js
+++ vue-learn/src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueDraggableResizable from 'vue-draggable-resizable'
 
+// optionally import default styles
+import 'vue-draggable-resizable/dist/VueDraggableResizable.css'
+
+Vue.component('vue-draggable-resizable', VueDraggableResizable)
 Vue.config.productionTip = false
 
 new Vue({
